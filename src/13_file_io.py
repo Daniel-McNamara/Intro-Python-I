bfff6b4d3f19bca3f6e7cff4d17ff5fffe5d003c
daniel mcnamara <danmac8407@gmail.com>
2020-07-08 14:55:52 -0500

    finished after changing pc

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('./src/foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+f = open('./src/bar.txt', 'w')
+f.write('Apple\nBanana\nCoconut')
+f.close()
+
+with open('./src/bar.txt') as f2:
+    print(f2.read())
